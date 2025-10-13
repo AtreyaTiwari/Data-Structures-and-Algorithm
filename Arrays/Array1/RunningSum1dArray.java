6ebf636a431bac39fca4eaabca74df1b0a036f8a
Atreya Tiwari <atreyatiwari102@gmail.com>
2025-10-13 23:45:27 +0530

    Running Sum

---
+++ Arrays/Array1/RunningSum1dArray.java
@@ -0,0 +1,20 @@
+package Arrays.Array1;
+import java.util.Arrays;
+
+public class RunningSum1dArray {
+
+    public static void main(String[] args) {
+        int[] nums = {1,2,3,4};
+        System.out.println(Arrays.toString(runningSum(nums)));
+    }
+    public static int[] runningSum(int[] nums) {
+        int sum=0;
+        int[] arr=new int[nums.length];
+        for(int i=0;i<nums.length;i++){
+            sum=sum+nums[i];
+            arr[i]=sum;
+        }
+        return arr;
+    }
+
+}
