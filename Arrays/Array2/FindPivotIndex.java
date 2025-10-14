9fa9740b55783885fe309ffc6b6616f58740d035
Atreya Tiwari <atreyatiwari102@gmail.com>
2025-10-14 23:59:56 +0530

    Find equilibrium element in an array

---
+++ Arrays/Array2/FindPivotIndex.java
@@ -0,0 +1,32 @@
+package Arrays.Array2;
+
+public class FindPivotIndex {
+    public static void main(String[] args) {
+        int[] nums = {1,7,3,6,5,6};
+        System.out.println(pivotIndex(nums));
+    }
+    public static int pivotIndex(int[] nums) {
+        int n = nums.length;
+        int[] tempr = new int[n];
+        int[] templ = new int[n];
+
+        int leftSum = 0;
+        int rightSum = 0;
+
+        for(int i = 0; i < n ;i++){
+            leftSum += nums[i];
+            templ[i] = leftSum;
+        }
+        for(int i = n - 1; i >= 0 ; i--){
+            rightSum += nums[i];
+            tempr[i] = rightSum;
+        }
+        for(int i = 0; i < n; i++){
+            if(templ[i] == tempr[i]){
+                return i;
+            }
+        }
+        return -1;
+    }
+}
+
