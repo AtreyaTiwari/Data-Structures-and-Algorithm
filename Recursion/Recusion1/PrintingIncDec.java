d4c37ba30924e0452527c84a04cd368454149036
Atreya Tiwari <atreyatiwari102@gmail.com>
2025-10-31 14:31:05 +0530

    Printing Inc Dec

---
+++ Recursion/Recusion1/PrintingIncDec.java
@@ -0,0 +1,15 @@
+package Recursion.Recusion1;
+
+public class PrintingIncDec {
+    public static void main(String[] args) {
+        int n=7;
+        print(n);
+    }
+    private static void print(int n){
+        if(n<1){
+            return;
+        }
+        print(n-1);
+        System.out.print(n+" ");
+    }
+}
