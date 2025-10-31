5ee3383b3bc69654b24c85a152b3620230ae91a6
Atreya Tiwari <atreyatiwari102@gmail.com>
2025-10-31 14:46:12 +0530

    Count digits which divides num evenly

---
+++ Recursion/Recusion1/CountDigits.java
@@ -0,0 +1,23 @@
+package Recursion.Recusion1;
+
+public class CountDigits {
+    public static void main(String[] args) {
+        int n= 2446;
+        System.out.println(evenlyDivides(n));
+    }
+    private static int evenlyDivides(int n) {
+        // code here
+         return count(n,0,n);
+    }
+    private static int count(int N,int c,int n){
+        if(n<=0){
+            return c;
+        }
+        int dig=n%10;
+        if(dig!=0 && N%dig==0){
+            c++;
+        }
+        return count(N,c,n/10);
+        
+    }
+}
