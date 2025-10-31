388d752e69f2246f8ca0bc31609ea33150861f78
Atreya Tiwari <atreyatiwari102@gmail.com>
2025-10-31 14:50:11 +0530

    Sum of array elements

---
+++ Recursion/Recusion1/ArraySum.java
@@ -0,0 +1,19 @@
+package Recursion.Recusion1;
+
+public class ArraySum {
+    public static void main(String[] args) {
+        int[] arr={3,4,6,1,3,8,10};
+        System.out.println(arraySum(arr));
+    }
+    private static int arraySum(int arr[]) {
+        // code here
+        return findSum(0,arr,0);
+    }
+    private static int findSum(int i,int[] arr,int sum){
+        if(i==arr.length){
+            return sum;
+        }
+        sum+=arr[i];
+        return findSum(i+1,arr,sum);
+    }
+}
